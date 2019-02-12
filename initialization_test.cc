82048d9ced4b460eb0f91fb4b87a276ff7b79725
megangoodland <megangoodland@gmail.com>
2019-02-12 15:00:06 -0500

    Adding uneven_division test case

--- initialization_test.cc
+++ initialization_test.cc
@@ -24,13 +24,13 @@
 //}
 
 
-// Test for the initialization module, which performs:
+// Tests for the initialization module, which performs:
 // Uniform initialization of the simulation of ants walking on a table, subdivided into squares. 
 // >>> Want to test this function with 5 cases. Small tables, making sure the ants get distributed uniformly.
+
 // what does it do when the number can't be distributed absolutely evenly?
 
 BOOST_AUTO_TEST_CASE(basic_test){
-    // create two 2x2 rarrays, one to put in the function and another to compare with it
     int length = 2;
     rarray<int,2> function_test(length,length);
     // number we want to distribute is 16
@@ -48,16 +48,16 @@ BOOST_AUTO_TEST_CASE(basic_test){
 }
 
 
-BOOST_AUTO_TEST_CASE(larger_numbers_test){
+BOOST_AUTO_TEST_CASE(larger_numbers){
     // create two 5x5 rarrays, one to put in the function and another to compare with it
     int length = 5;
     rarray<int,2> function_test(length,length);
     rarray<int,2> compare(length,length); 
-    // number we want to distribute is 16
-    int n = 1000;
+    // number we want to distribute is n
+    int n = 10000;
     // f is the number that should appear in each square
     int f = n/(length*length);
-    // fill compare array with 4's
+    // fill compare array with f
     compare.fill(f);
     // run function with test array and n
     initialize_uniform(function_test, n);
@@ -67,15 +67,39 @@ BOOST_AUTO_TEST_CASE(larger_numbers_test){
     for (int i=0; i<length; i++) {
         for (int j=0; j<length; j++){
             if (function_test[i][j] == compare[i][j]){
-                test_result = 1;
-            }
+                test_result = 1;}
             else {
-                test_result = 0;
-            }
-
+                test_result = 0;}
         }
     }
-    BOOST_CHECK_MESSAGE(test_result == 1, "function initialize_uniform in larger_numbers_test");
+    BOOST_CHECK_MESSAGE(test_result == 1, "function initialize_uniform in larger_numbers");
 }
 
+
+BOOST_AUTO_TEST_CASE(uneven_division){
+    // create two 5x5 rarrays, one to put in the function and another to compare with it
+    int length = 5;
+    rarray<int,2> function_test(length,length);
+    rarray<int,2> compare(length,length); 
+    // number we want to distribute is n
+    int n = 10001;
+    // f is the number that should appear in each square
+    int f = n/(length*length);
+    // fill compare array with f
+    compare.fill(f);
+    // run function with test array and n
+    initialize_uniform(function_test, n);
+    // check if the arrays are the same
+    // fill compare array with difference between old value and corresponding element from function_test
+    int test_result = 1; // 1 means pass, 0 means fail
+    for (int i=0; i<length; i++) {
+        for (int j=0; j<length; j++){
+            if (function_test[i][j] == compare[i][j]){
+                test_result = 1;}
+            else {
+                test_result = 0;}
+        }
+    }
+    BOOST_CHECK_MESSAGE(test_result == 1, "function initialize_uniform in uneven_division");
+}
 // could also just go through every element, check if they're all the same, and then check if they add up to n?
